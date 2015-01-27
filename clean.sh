f3af052acdddf16a2ff1376b916c4da07fc3a638
Rebecca Voglewede <rebecca.voglewede@gmail.com>
2015-01-27 11:36:43 -0600

    Add clean.sh script

---
+++ clean.sh
@@ -0,0 +1,22 @@
+# This script peforms cleaning operations of country files with errors
+
+cd data/clean
+
+# Fix Guinea file - we just want the middle 12 rows
+echo "Fixing Guinea.cc.txt file"
+head -n 24 Guinea.cc.txt | tail -n 12 > Guinea.cc.txt.clean
+mv -v Guinea.cc.txt.clean Guinea.cc.txt
+
+# Fix China file - we just want the top twelve rows
+echo "Fixing China.cc.txt file"
+head -n 12 China.cc.txt > China.cc.txt.clean
+mv -v China.cc.txt.clean China.cc.txt
+
+# Fix Niger file - we just want the top twelve rows
+echo "Fixing Niger.cc.txt file"
+head -n 12 Niger.cc.txt > Niger.cc.txt.clean
+mv -v Niger.cc.txt.clean Niger.cc.txt
+
+cd ../..
+
+sleep 1
